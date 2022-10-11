8c441e62ec319121470b585fc56a1aa0906ec417
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-10-11 13:29:07 +0900

    [update] コメントの修正

--- Assets/Script/Editor extension/InputNameDropBox.cs
+++ Assets/Script/Editor extension/InputNameDropBox.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 using System.Collections.Generic;
 
 /// <summary>
-/// インスペクタにタグ一覧を表示するエディタ拡張
+/// インスペクタにInputManagerのName一覧を表示するエディタ拡張
 /// </summary>
 public class InputNameAttribute : PropertyAttribute { }
 
