527b6579b10b1d02a26892df0ac2578481e8cb5f
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-10-19 18:03:10 +0900

    [update] プレイヤーウエポンの更新,コンセプトテキストの変更有り。

---
+++ Assets/Script/PlayerWeapon/Weapons/00Bases/Base_Bullet.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 弾にアタッチすべきコンポーネントの基底クラス。<br/>
+/// </summary>
+public abstract class Base_Bullet : MonoBehaviour, IOnEnemyDamage
+{
+    #region Properties
+    #endregion
+
+    #region Inspector Variables
+    [TagName, SerializeField]
+    private string _enemyTagName = "";
+    #endregion
+
+    #region Member Variables
+    // Components
+    private Animator _animator;
+
+    // Athor Variables
+    protected readonly BulletType _bulletType = BulletType.NOT_SET;
+    protected readonly HitType _hitType = HitType.NOT_SET;
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        Init();
+    }
+    private void Update()
+    {
+        Process();
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Protected Methods
+    #endregion
+
+    #region Private Methods
+    private void Process()
+    {
+        switch (_bulletType)
+        {
+            case BulletType.NOT_SET:
+                Debug.LogError("<color=grey>_bulletTypeが未設定です！修正してください！</color>");
+                break;
+            case BulletType.MOVE:
+                Move();
+                break;
+            case BulletType.NOT_MOVE:
+                break;
+            case BulletType.SPECIAL:
+                Special();
+                break;
+            default:
+                Debug.LogError("<color=grey>_bulletTypeに不正な値が入っています！修正してください！</color>");
+                break;
+        }
+    }
+    #endregion
+
+    #region Virtual Methods
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    protected virtual void Init()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    protected virtual void Move() { Debug.LogWarning("<color=grey>継承し中身を実装してください！</color>"); }
+    /// <summary>
+    /// 特殊武器処理
+    /// </summary>
+    protected virtual void Special() { Debug.LogWarning("<color=grey>継承し中身を実装してください！</color>"); }
+    /// <summary>
+    /// 攻撃処理 <br/>
+    /// オーバーライド可 <br/>
+    /// </summary>
+    /// <param name="enemyStatusManager"> 攻撃相手のステータス管理クラス </param>
+    /// <param name="value"> 減らすライフの量 </param>
+    /// <param name="isKnockback"> ノックバックするかどうか </param>
+    /// <param name="_rigidbody"> ノックバックさせる場合に攻撃相手のRigidbodyをここに設定する。</param>
+    /// <param name="dir"> ノックバックさせる場合にノックバック方向をここに設定する。</param>
+    /// <param name="knockbackPower"> ノックバックの強さ </param>
+    public virtual void OnDamage(
+        EStatusManager enemyStatusManager, float value, bool isKnockback = false,
+        Rigidbody _rigidbody = null, Vector3 dir = default, float knockbackPower = 1.0f)
+    {
+        // 体力を減らす処理
+        var temp = enemyStatusManager.Status;
+        temp._life -= value;
+        enemyStatusManager.Status = temp;
+
+        // ノックバック処理
+        if (isKnockback)
+        {
+            if (_rigidbody == null)
+            {
+                Debug.LogError(
+                    $"<color=grey>_rigidbodyがnullです！修正してください！</color>");
+            }
+            else
+            {
+                _rigidbody.AddForce(dir.normalized * knockbackPower, ForceMode.Impulse);
+            }
+        }
+    }
+    #endregion
+
+    #region On Animation Events
+    /// <summary>
+    /// このゲームオブジェクトを破棄する。<br/>
+    /// アニメーションイベントから呼び出す想定で作成したメソッド。
+    /// </summary>
+    private void Destroy()
+    {
+        Destroy(gameObject);
+    }
+    #endregion
+}
---
+++ Assets/Script/PlayerWeapon/Weapons/00Bases/BulletType.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 弾の種類を表すenum
+/// </summary>
+[HideInInspector]
+public enum BulletType
+{
+	/// <summary> 未設定 </summary>
+	NOT_SET,
+	/// <summary> 飛ぶもの / 動くもの </summary>
+	MOVE,
+	/// <summary> 近距離攻撃 / 飛ばないもの / 生まれた場所から動かないもの </summary>
+	NOT_MOVE,
+	/// <summary> その他 / 召喚モノとか？ </summary>
+	SPECIAL,
+}
+
+public enum HitType
+{
+	/// <summary> 未設定 </summary>
+	NOT_SET,
+	/// <summary> コライダー衝突時のみ </summary>
+	ON_COLLISION_ENTER,
+	/// <summary> コライダー衝突中ずっと </summary>
+	ON_COLLISION_STAY,
+	/// <summary> トリガー接触時のみ </summary>
+	ON_TRIGGER_ENTER,
+	/// <summary> トリガー接触中ずっと </summary>
+	ON_TRIGGER_STAY,
+	/// <summary> レイ接触 </summary>
+	RAY_HIT_,
+
+	/// <summary> その他 </summary>
+	ATHER,
+}
