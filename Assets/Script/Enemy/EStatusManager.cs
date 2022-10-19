d10f1bcd61256329b2eca8c30b7d3be35380805d
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-10-19 17:59:25 +0900

    [update] エネミのステータス管理を修正

--- Assets/Script/Enemy/EStatusManager.cs
+++ Assets/Script/Enemy/EStatusManager.cs
@@ -8,9 +8,13 @@ using UnityEngine;
 public class EStatusManager : MonoBehaviour
 {
     #region Properties
+    public EStatus Status { get => _status; set => _status = value; }
     #endregion
 
     #region Inspector Variables
+    [Header("エネミーに設定すべき値")]
+    [Tooltip(""), SerializeField]
+    private EStatus _status = default;
     #endregion
 
     #region Member Variables
@@ -21,17 +25,17 @@ public class EStatusManager : MonoBehaviour
 
     #region Events
     #endregion
-    
+
     #region Unity Methods
 
     private void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+
     }
 
     #endregion
