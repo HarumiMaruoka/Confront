0eff65d1246672a33ee4664a62d535ab11326e2c
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-02-21 12:12:35 +0900

    [update] 攻撃ちょっと更新

--- Assets/Script/Player/01StateMachine/PlayerStateMachine.cs
+++ Assets/Script/Player/01StateMachine/PlayerStateMachine.cs
@@ -119,7 +119,7 @@ namespace Player
             _attackStateController.Init(this);
 
             // =================== テスト用処理 =================== //
-            _attackStateController.SetBothState(0, 2);
+            _attackStateController.SetBothState(1, 3);
             // ==================================================== //
         }
         protected override void StateInit()
