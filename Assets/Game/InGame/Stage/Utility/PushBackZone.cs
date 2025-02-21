58c202739d249fb8476e629820217a398bd26642
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2025-02-21 14:40:07 +0900

    こうしん

---
+++ Assets/Game/InGame/Stage/Utility/PushBackZone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Confront.Stage.Utility
+{
+    public class PushBackZone : MonoBehaviour
+    {
+        [SerializeField] private float _height = 0.0f;
+
+        private void Update()
+        {
+            var player = Player.PlayerController.Instance;
+            if (!player) return;
+            if (player.transform.position.y < _height)
+            {
+                var prev = player.CharacterController.enabled;
+                player.CharacterController.enabled = false;
+                player.transform.position = new Vector3(player.transform.position.x, _height, player.transform.position.z);
+                player.CharacterController.enabled = prev;
+            }
+        }
+    }
+}
